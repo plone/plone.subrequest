71b993af4fab3cf85ccd8c24a450b69584ef95fe
Maurits van Rees <maurits@vanrees.org>
2016-02-09 21:24:41 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -7,35 +7,41 @@ setup(
     name='plone.subrequest',
     version=version,
     description='Subrequests for Zope2',
-    long_description=open("README.rst").read() + "\n\n" +
-        open(os.path.join('plone', 'subrequest', 'usage.txt')).read() + "\n\n" +
-        open("CHANGES.rst").read(),
+    long_description=(
+        open("README.rst").read() + "\n\n" +
+        open(os.path.join('plone', 'subrequest', 'usage.txt')).read() +
+        "\n\n" +
+        open("CHANGES.rst").read()),
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
+        "Framework :: Plone : 4.3",
+        "Framework :: Plone : 5.0",
         "Framework :: Zope2",
         'Intended Audience :: Developers',
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        ],
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
     keywords='plone',
     author='Plone Foundation',
     author_email='plone-developers@lists.sourceforge.net',
-    url='http://pypi.python.org/pypi/plone.subrequest/',
+    url='https://pypi.python.org/pypi/plone.subrequest/',
     license='GPL version 2',
-    packages = find_packages(),
-    namespace_packages = ['plone'],
-    include_package_data = True,
-    platforms = 'Any',
-    zip_safe = False,
+    packages=find_packages(),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    platforms='Any',
+    zip_safe=False,
     install_requires=[
         # 'Acquisition',
         'five.globalrequest',
         'setuptools',
         'zope.globalrequest',
         ],
-    extras_require = {
+    extras_require={
         'test': [
             'five.localsitemanager',
             'manuel',
