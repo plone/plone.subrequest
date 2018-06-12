b901f248d32fc0d703fe52b8a7f364b41b9479bf
Johannes Raggam <thetetet@gmail.com>
2018-06-12 21:47:12 +0200

    cosmetics

--- plone/subrequest/subresponse.py
+++ plone/subrequest/subresponse.py
@@ -13,7 +13,7 @@ except ImportError:
 class SubResponse(HTTPResponse):
 
     def setBody(self, body, title='', is_error=0, **kw):
-        """ Accept either a stream iterator or a string as the body """
+        """Accept either a stream iterator or a string as the body."""
         if not IStreamIterator.providedBy(body):
             return HTTPResponse.setBody(self, body, title, is_error, **kw)
         assert not self._wrote
@@ -36,13 +36,13 @@ class SubResponse(HTTPResponse):
         return str(self.body)
 
     def outputBody(self):
-        """Output the response body"""
+        """Output the response body."""
         if not self._wrote:
             self.stdout.write(self.body)
             self._wrote = 1
 
     def getBody(self):
-        """ Return the body, however it was written. """
+        """Return the body, however it was written."""
         if not self._wrote:
             return self.body
         stdout = self.stdout
