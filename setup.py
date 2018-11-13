827618a798e3034d2fc84fa2d879563cc2b3c762
Gil Forcada Codinachs <gil.gnome@gmail.com>
2018-11-13 22:03:26 +0100

    Remove Plone 4.3..5.1 trove classifiers
    Versions from 1.8.x branch are compatible with those Plone versions.
    
    master is now only compatible with Plone 5.2 only.

--- setup.py
+++ setup.py
@@ -19,9 +19,6 @@ setup(
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
-        "Framework :: Plone :: 4.3",
-        "Framework :: Plone :: 5.0",
-        "Framework :: Plone :: 5.1",
         "Framework :: Plone :: 5.2",
         "Framework :: Zope2",
         "Intended Audience :: Developers",
